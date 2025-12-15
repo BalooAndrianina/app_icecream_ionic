591a439634a49e8dfc83933b8ef2c2242d86a8c6
BalooAndrianina <b.andrianina@gmail.com>
2025-12-15 11:54:54 +0100

    (feat): ajout du bouton MakeIceCream, non fonctionnelle encore, message d'erreur si plus de 5 boules selectionnée

--- src/app/views/make-ice-cream/make-ice-cream.page.ts
+++ src/app/views/make-ice-cream/make-ice-cream.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonButton } from '@ionic/angular/standalone';
 
 import { StockRepository } from 'src/app/repository/stock-repository';
 import { Flavour } from 'src/app/data/flavour';
@@ -19,7 +19,7 @@ import { ExtrasSelectorComponent } from '../components/extras-selector/extras-se
   imports: [
     IonContent, IonHeader, IonTitle, IonToolbar, IonGrid,
     CommonModule, FormsModule,
-    FlavourProductComponent, ContainerSelectorComponent, ExtrasSelectorComponent
+    FlavourProductComponent, ContainerSelectorComponent, ExtrasSelectorComponent, IonButton
   ]
 })
 export class MakeIceCreamPage implements OnInit {
@@ -50,8 +50,64 @@ export class MakeIceCreamPage implements OnInit {
     hazelnuts: false
   };
 
+  // Calcul du nombre total de boules sélectionnées
+  get totalScoops(): number {
+    return this.flavours.reduce(
+      (sum, flavour) => sum + flavour.quantity,
+      0
+    );
+  }
+  // Calcul du prix total des boules sélectionnées
+  get scoopsPrice(): number {
+    const scoops = this.totalScoops;
 
+    switch (scoops) {
+      case 1: return 1.5;
+      case 2: return 3;
+      case 3: return 4;
+      case 4: return 5;
+      case 5: return 5.5;
+      default: return 0;
+    }
+  }
 
+// Calcul du prix DES contenants sélectionnés
+  get containerPrice(): number {
+    return this.selectedContainer === 'cone' ? 1 : 0;
+  }
+
+  // Calcul du prix DES extras sélectionnés
+  get extrasPrice(): number {
+    let price = 0;
+
+    if (this.selectedExtras.whippedCream) {
+      price += 0.95;
+    }
+
+    if (this.selectedExtras.hazelnuts) {
+      price += 0.95;
+    }
+
+    return price;
+  }
+
+  // Calcul du prix total de la commande
+  get totalPrice(): number {
+    return (
+      this.scoopsPrice +
+      this.containerPrice +
+      this.extrasPrice
+    );
+  }
+
+  // Vérification si la commande peut être passée
+  get canMakeIceCream(): boolean {
+    return this.totalScoops > 0 && this.totalScoops <= 5;
+  }
+
+  get tooManyScoops(): boolean {
+    return this.totalScoops > 5;
+  }
 
   ngOnInit() {}
 }
